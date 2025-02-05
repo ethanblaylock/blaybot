e93ac51b6ea5a5c76d62e89854f951dde04d23d9
Ethan Blaylock <ethan.blaylock37@gmail.com>
2025-02-04 18:39:59 -0700

    interfaced wireless controller with the nodes and verified that it works

--- robot_ws/src/mobility/mobility/joystick_node.py
+++ robot_ws/src/mobility/mobility/joystick_node.py
@@ -13,7 +13,6 @@ class JoystickNode(Node):
         self.xbox_publisher = self.create_publisher(Xbox, '/xbox', 10)
 
     def joy_callback(self, msg):
-        self.get_logger().info('Received Joy message: %s' % msg)
 
         # Create Xbox message
         xbox_msg = Xbox()
@@ -21,25 +20,26 @@ class JoystickNode(Node):
         # Assign values from Joy axes to Xbox message
         xbox_msg.l_stick_lr= msg.axes[0]
         xbox_msg.l_stick_ud = msg.axes[1]
-        xbox_msg.l_trigger = msg.axes[2]
-        xbox_msg.r_stick_lr = msg.axes[3]
-        xbox_msg.r_stick_ud = msg.axes[4]
-        xbox_msg.r_trigger = msg.axes[5]
+        xbox_msg.r_stick_lr = msg.axes[2]
+        xbox_msg.r_stick_ud = msg.axes[3]
+        xbox_msg.r_trigger = msg.axes[4]
+        xbox_msg.l_trigger = msg.axes[5]
         xbox_msg.d_pad_lr = msg.axes[6]
         xbox_msg.d_pad_ud = msg.axes[7]
 
         # Assign values from Joy buttons to Xbox message
         xbox_msg.a = msg.buttons[0]
         xbox_msg.b = msg.buttons[1]
-        xbox_msg.x = msg.buttons[2]
-        xbox_msg.y = msg.buttons[3]
-        xbox_msg.l_bumper = msg.buttons[4]
-        xbox_msg.r_bumper = msg.buttons[5]
-        xbox_msg.back = msg.buttons[6]
-        xbox_msg.start = msg.buttons[7]
-        xbox_msg.xbox= msg.buttons[8]
-        xbox_msg.l_stick_press = msg.buttons[9]
-        xbox_msg.r_stick_press = msg.buttons[10]
+        xbox_msg.x = msg.buttons[3]
+        xbox_msg.y = msg.buttons[4]
+        xbox_msg.l_bumper = msg.buttons[6]
+        xbox_msg.r_bumper = msg.buttons[7]
+        xbox_msg.view = msg.buttons[10]
+        xbox_msg.menu = msg.buttons[11]
+        xbox_msg.xbox= msg.buttons[12]
+        xbox_msg.l_stick_press = msg.buttons[13]
+        xbox_msg.r_stick_press = msg.buttons[14]
+        xbox_msg.share = msg.buttons[15]
 
         # Publish Xbox message
         self.xbox_publisher.publish(xbox_msg)
@@ -54,7 +54,8 @@ def main(args=None):
         rclpy.spin(node)
     except KeyboardInterrupt:
         pass
-    except:
+    except Exception as e:
+        print(e)
         rclpy.shutdown()
     finally:
         # Destroy the node explicitly
