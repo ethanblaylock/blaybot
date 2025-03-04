c8d7889cbf9e8fbc2436f53ebd80bccbe4ef1036
Ethan M. Blaylock <ethan.blaylock37@gmail.com>
2025-03-04 15:51:18 -0700

    add in arm launch file

---
+++ robot_ws/src/arm_control/launch/arm_control_launch.py
@@ -0,0 +1,15 @@
+import launch
+from launch import LaunchDescription
+from launch.actions import LogInfo, DeclareLaunchArgument
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        # Start arm_node
+        Node(
+            package='arm_control',  
+            executable='arm_node',  # Adjust this to your node's executable
+            name='arm_node',
+            output='screen',
+        ),
+    ])
