cff2f0b2e20e2fd2021d52583395c870fe2a0aa3
Ethan Blaylock <ethan.blaylock37@gmail.com>
2025-03-28 10:45:27 -0600

    changed starting angles

--- robot_ws/src/arduino_mega/arduino_mega.ino
+++ robot_ws/src/arduino_mega/arduino_mega.ino
@@ -204,7 +204,7 @@ void setupTimer3() {
 
     ICR3 = 39999;  // (16,000,000 / (50 * 8)) - 1 = 39999
 
-    OCR3A = 3000;  // Start with 7.5% duty cycle
+    OCR3A = 2500;  // Start with 7.5% duty cycle
     OCR3B = 3000;  // Start with 7.5% duty cycle
     OCR3C = 3000;  // Start with 7.5% duty cycle
 }
@@ -221,9 +221,9 @@ void setupTimer5() {
 
     ICR5 = 39999;  // (16,000,000 / (50 * 8)) - 1 = 39999
 
-    OCR5A = 3000;  // Start with 7.5% duty cycle
+    OCR5A = 3500;  // Start with 7.5% duty cycle
     OCR5B = 3000;  // Start with 7.5% duty cycle
-    OCR5C = 3000;  // Start with 7.5% duty cycle
+    OCR5C = 3100;  // Start with 7.5% duty cycle
 }
 
 // Function to set Drive PWM duty cycle
